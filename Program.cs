b9f90d16e45b84622d51c0cd0a9635f44c6b05de
Ranindu Amarasinghe <91935675+RaninduAmarasinghe@users.noreply.github.com>
2023-02-21 13:00:43 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace User_is_eligible_for_vote;
+class Program
+{
+    static void Main(string[] args)
+    {
+        String name;
+        int age;
+        Console.WriteLine("Enter ur name");
+        name=Console.ReadLine();
+
+        Console.WriteLine("Enter ur age");
+        age = int.Parse(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine(name + "ur eligible for voting");
+        }
+        else
+        {
+            Console.WriteLine(name + "ur not eligible for voting");
+        }
+        Console.ReadKey();
+    }
+}
+
